14eebf4fdd20bc698f0bac725ff78eb3f70d1205
JohnnyB87 <johnbrady1987@gmail.com>
2018-03-24 16:56:08 +0000

    Stopped using controllers

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,13 @@
-
-import controllers.GameTab;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     private int windowWidth = 400;
-    private int windowHeight = 400;
+    private int windowHeight = 350;
 
     public static void main(String[] args){
         launch(args);
@@ -22,17 +16,30 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         primaryStage.setTitle("Window");
+//        primaryStage.setMaxHeight(windowHeight);
+//        primaryStage.setMaxWidth(windowWidth);
+//        primaryStage.setMinHeight(windowHeight);
+//        primaryStage.setMinWidth(windowWidth);
         primaryStage.setResizable(false);
 
-        Tab tab = new GameTab("Game Tab","GUESS", "#dedede");
+        TabPane tabPane = new TabPane();
 
+        String colour = "#bcbcbc";
+        GuessingGameTab guessingGameTab = new GuessingGameTab("Guessing Game","Guess", colour);
+        LotteryGameTab lotteryGameTab = new LotteryGameTab("Lottery Game","Submit", colour);
+        
+        guessingGameTab.setClosable(false);
+        lotteryGameTab.setClosable(false);
+        
+        tabPane.getTabs().add(guessingGameTab);
+        tabPane.getTabs().add(lotteryGameTab);
+        guessingGameTab.showPrizeTab();
 
-        TabPane tp = new TabPane();
-        tp.getTabs().add(tab);
-        StackPane sp = new StackPane();
-        sp.getChildren().add(tp);
+        StackPane layout = new StackPane();
+        layout.getChildren().add(tabPane);
+        
+        Scene scene = new Scene(layout, windowWidth, windowHeight);
 
-        Scene scene = new Scene(sp,windowWidth,windowHeight);
         primaryStage.setScene(scene);
         primaryStage.show();
 
