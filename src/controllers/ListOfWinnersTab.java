4c2c65d1e9115325083a1fac75534385fb0c19b2
JohnnyB87 <johnbrady1987@gmail.com>
2018-04-10 15:32:11 +0100

    messing around

--- src/controllers/ListOfWinnersTab.java
+++ src/controllers/ListOfWinnersTab.java
@@ -1,7 +1,10 @@
 package controllers;
 
+import javafx.event.ActionEvent;
 import javafx.scene.control.Tab;
 
 public class ListOfWinnersTab extends Tab {
 
+    public void removeButtonPressed() {
+    }
 }
